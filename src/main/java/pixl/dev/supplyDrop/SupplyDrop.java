c6962bf3fbe07029a7ee3df0dda7dfa6ccc63477
Hyacinthvs <hyacinthusyt@gmail.com>
2026-08-11 14:53:20 -0400

    Initial commit

---
+++ src/main/java/pixl/dev/supplyDrop/SupplyDrop.java
@@ -0,0 +1,16 @@
+package pixl.dev.supplyDrop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SupplyDrop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
