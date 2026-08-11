304de8b2c434aa84d4d9c685ee7100291f4eaa1f
Hyacinthvs <hyacinthusyt@gmail.com>
2026-08-11 16:28:40 -0400

    Finished randomized drop location

--- src/main/java/pixl/dev/supplyDrop/Commands/SupplyDropCommand.java
+++ src/main/java/pixl/dev/supplyDrop/Commands/SupplyDropCommand.java
@@ -1,4 +1,41 @@
 package pixl.dev.supplyDrop.Commands;
 
-public class SupplyDropCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class SupplyDropCommand implements CommandExecutor, TabCompleter {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p = (Player) sender;
+
+        if (cmd.getName().equalsIgnoreCase("supplydrop")) {
+            // Normal player permission command chain
+            if (p.hasPermission("supplydrop.player")) {
+
+            }
+            // Admin permission command chain
+            if (p.hasPermission("supplydrop.admin")) {
+
+            }
+        }
+
+        return true;
+    }
+    // Autocompleter for commands
+    public ArrayList<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args){
+        ArrayList<String> completions = new ArrayList<>();
+
+        if (args.length == 1) {
+
+        }
+        if (args.length == 2) {
+
+        }
+        return completions;
+    }
 }
--- src/main/java/pixl/dev/supplyDrop/Drop/DropLocation.java
+++ src/main/java/pixl/dev/supplyDrop/Drop/DropLocation.java
@@ -1,4 +1,75 @@
 package pixl.dev.supplyDrop.Drop;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import pixl.dev.supplyDrop.Main;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class DropLocation {
+
+    private final Main plugin;
+
+    public DropLocation(Main plugin) {
+        this.plugin = plugin;
+    }
+    private int getMaxDistance(){
+        int maxDistance = plugin.getConfig().getInt("spawn.max_distance");
+        return maxDistance;
+    }
+
+    private boolean isDropLocationSafe(Location location){
+        Material ground = location.clone().subtract(0,1,0).getBlock().getType();
+
+        if (!ground.isSolid()){
+            return false;
+        }
+        if (ground == Material.AIR||ground == Material.WATER||ground==Material.FIRE||ground==Material.SOUL_FIRE){
+            return false;
+        }
+        if (location.getBlock().getType()==Material.AIR){
+            return false;
+        }
+
+        // Should all these checks succeed, it's good to go!
+        return true;
+    }
+
+    private Location getRandomDropLocation(World world){
+        // Gets a random location. NOT FINAL. Doesn't check if it is safe (that is what getdroplocation is for)
+
+        Location spawn = world.getSpawnLocation();
+        int maxDistance = getMaxDistance();
+        // Locally generated random number generator
+        ThreadLocalRandom random = ThreadLocalRandom.current(); // Note to self, this is new to me so I might be misunderstanding it
+        int x = random.nextInt(spawn.getBlockX()-maxDistance, spawn.getBlockX()+maxDistance);
+        int z = random.nextInt(spawn.getBlockZ()-maxDistance, spawn.getBlockZ()+maxDistance);
+        int y = world.getHighestBlockYAt(x, z);
+
+        Location location = new Location(world, x, y, z);
+        return location;
+    }
+
+    public Location getDropLocation(){
+        World world =  Bukkit.getWorld(plugin.getConfig().getString("settings.world"));
+        if (world == null){
+            // This should not occur because it's fairly easy to type in a world name but you never know :/
+            plugin.getLogger().warning("World not found!");
+            return null;
+        }
+
+        for (int i=0; i<10; i++){
+            Location loc = getRandomDropLocation(world);
+
+            if (isDropLocationSafe(loc)){
+                return loc;
+            }
+        }
+        // If it exits the loop without finding a good drop location
+        plugin.getLogger().warning("Could not find any drop location!");
+        return null;
+    }
+
 }
